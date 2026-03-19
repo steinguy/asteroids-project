f0209e59ffd21488dfc391ccf9d2ae1481792182
steinguy <michaelfsteinacker@gmail.com>
2026-03-18 21:36:45 -0400

    add clock

--- main.py
+++ main.py
@@ -10,7 +10,9 @@ def main():
     pygame.init()
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         log_state()
         for event in pygame.event.get():
@@ -18,6 +20,9 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        clock.tick(60)
+        dt = clock.tick(60) / 1000
+        
 
 
     
