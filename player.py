38116280ced80f58927002b3db16f62a011c3521
steinguy <michaelfsteinacker@gmail.com>
2026-04-07 14:47:07 -0400

    Add move method

--- player.py
+++ player.py
@@ -3,6 +3,7 @@ from circleshape import CircleShape
 from constants import PLAYER_RADIS
 from constants import LINE_WIDTH
 from constants import PLAYER_TURN_SPEED
+from constants import PLAYER_SPEED
 
 class Player(CircleShape):
     def __init__(self, x, y):
@@ -24,6 +25,12 @@ class Player(CircleShape):
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
     
+    def move(self, dt):
+        unit_vector = pygame.Vector2(0, 1)
+        rotated_vector = unit_vector.rotate(self.rotation)
+        rotated_with_speed_vector = rotated_vector * PLAYER_SPEED * dt
+        self.position += rotated_with_speed_vector
+
     def update(self, dt):
         keys = pygame.key.get_pressed()
 
@@ -31,4 +38,9 @@ class Player(CircleShape):
                 self.rotate(-dt)
         if keys[pygame.K_d]:
                 self.rotate(dt)
+        if keys[pygame.K_w]:
+              self.move(dt)
+        if keys[pygame.K_s]:
+              self.move(-dt)
         
+    
